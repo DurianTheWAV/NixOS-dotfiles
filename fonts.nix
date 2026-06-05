8d563e1a06107c49d5db48d1f67b360f75616bd2
Durian <dorian@luyet.ch>
2026-06-05 15:23:09 +0200

    Problematic fonts removed

--- fonts.nix
+++ fonts.nix
@@ -6,13 +6,9 @@
   # --- Fonts ---
   fonts.fontconfig.enable = true;
     fonts.packages = with pkgs; [
-    # Polices universelles (inclut le coréen)
+    # Universal fonts
     noto-fonts
     noto-fonts-cjk-sans
     noto-fonts-cjk-serif
-  
-    # Polices coréennes spécifiques et populaires
-    nanum
-    baekmuk-ttf
   ];
 }
